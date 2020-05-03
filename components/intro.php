a50dc3c3231db8bb87d04a5a99e2450b7d2060bc
Petr Stoklas <petrstoklas@gmail.com>
2020-05-03 20:48:15 +0200

    Page separated into smaller components.

---
+++ components/intro.php
@@ -0,0 +1,15 @@
+<div class="intro" id=#intro>  <!-- INTRODUCTION SECTION-->
+    <div class="ps">
+        <p class="first">
+            I'm Peter, your Web Designer and I will make your online media shine.
+        </p>
+        <br>
+        <p class="second">
+        What I love is scuba-diving, skiing and highly functional websites with a clean design. Just <a href="#contact-wrap">contact</a> me so we can start developing your responsive web site.
+        </p>
+        <br>
+        <div class="third">
+            <a href="#reasons"> 3 reasons why to invest into your website </a>
+        </div>
+    </div>  
+</div>
---
+++ components/reasons.php
@@ -0,0 +1,15 @@
+<h2>3 reasons to invest into your website</h2>
+
+<p>
+Online marketplace is inevitably growing and thus it is important to keep track with it or do a step forward. One of the tricks is SEO which if done well makes your website more accessible via search engine. Result of this is having more visitors.
+</p>
+<br>
+
+<p class='middle-reason'>
+Once your website is easy to find, it is important that your site is designed responsively so people can enjoy your online channel not only on their desktop computers but mainly smartphones as the number of mobile web visits is outgrowing the one of visits made from desktops.
+</p>
+<br>
+
+<p>
+Now you are having amazing website but how is the logic behind it? It is good idea to keep it as simple as possible not only because of easy maintanance later but also to reduce energy costs. 
+</p>
