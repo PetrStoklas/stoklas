8dba2250dd0653dbfd3a94e1b0aae0e87932c372
Petr Stoklas <petrstoklas@gmail.com>
2020-05-04 12:18:42 +0200

    Mail updated.

--- myMailScrpt.php
+++ myMailScrpt.php
@@ -7,10 +7,10 @@
         isset($_POST['sname'])
         ) {
         $message = $_POST['long_text'];
-        $headers = "from: tynabroz@gmail.com \n";
+        $headers = "from: {$_POST['mail']} \n";
         $headers .= "X-mailer: phpWebmail \n";
 
-        $infoHeaders = "from: {$_POST['mail']} \n";
+        $infoHeaders = "from: petrstoklas@gmail.com \n";
         $infoMessage = "Thank you for contacting Needaweb. You will get an answer within few days.";
         mail("petrstoklas@gmail.com", "Message from Needaweb portfolio.", $message, $headers);
         mail($_POST['mail'], "Your message to Needaweb.", $infoMessage, $infoHeaders);
