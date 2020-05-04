40a5445c66ec437cffdbe0404fc9acb765f6f197
Petr Stoklas <petrstoklas@gmail.com>
2020-05-04 13:23:02 +0200

    Mail auto resposne deleted.

--- myMailScrpt.php
+++ myMailScrpt.php
@@ -10,9 +10,6 @@
         $headers = "from: {$_POST['mail']} \n";
         $headers .= "X-mailer: phpWebmail \n";
 
-        $infoHeaders = "from: petrstoklas@gmail.com \n";
-        $infoMessage = "Thank you for contacting Needaweb. You will get an answer within few days.";
         mail("petrstoklas@gmail.com", "Message from Needaweb portfolio.", $message, $headers);
-        mail($_POST['mail'], "Your message to Needaweb.", $infoMessage, $infoHeaders);
     }
 ?>
