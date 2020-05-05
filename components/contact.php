b9554b7efdf9c4332b40115e182ce1868c99c4bd
Petr Stoklas <petrstoklas@gmail.com>
2020-05-05 13:12:09 +0200

    Redirect back.

--- components/contact.php
+++ components/contact.php
@@ -17,7 +17,7 @@
         
         else $statusTwo = "2";
 
-        // header("Location: http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+        header("Location: http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
 
         // sleep(2);
 
