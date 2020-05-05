bd270bf4a32a68c10bb16bbc1dce8a2facd52c91
Petr Stoklas <petrstoklas@gmail.com>
2020-05-05 11:29:52 +0200

    Testing echo.

--- components/contact.php
+++ components/contact.php
@@ -10,7 +10,7 @@
         <textarea class="lng-txt" name="long_text" placeholder="Type your message here..." label="message content input"></textarea>
         
         <button class="submit-btn" type="submit" name="submit">Send Me Message!</button>
-        <?php echo $status ?>
+        <?php echo "KOKOS" ?>
     </div>   
     
     <svg class="logo-bottom" width="100%" height="100%" viewBox="0 0 518 355" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;">
